bcb4a98d66a362ea1d43e81255c06c1428f5fbe6
Bruno Menegidio <bruno.silva@conecta.tech>
2023-06-17 11:55:36 -0300

    feat: enable transform and cors

--- apps/coins-api/src/main.ts
+++ apps/coins-api/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -6,7 +6,15 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
+  );
+  app.enableCors();
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
