8017da4fb239a99f6d123a599c4c64a63eb547aa
Bruno Menegidio <bruno.silva@conecta.tech>
2023-06-17 16:32:16 -0300

    feat: apply ui-kitten

--- apps/coins-app/src/app/App.tsx
+++ apps/coins-app/src/app/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout } from '@ui-kitten/components';
+
+import { default as theme } from '../theme/ui-kitten-theme.json';
 
 import AssetsPage from './pages/assets';
 
 export const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <AssetsPage />
-      </SafeAreaView>
-    </>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+      <StatusBar barStyle="light-content" />
+      <Layout style={{ flex: 1 }}>
+        <SafeAreaView>
+          <AssetsPage />
+        </SafeAreaView>
+      </Layout>
+    </ApplicationProvider>
   );
 };
 
