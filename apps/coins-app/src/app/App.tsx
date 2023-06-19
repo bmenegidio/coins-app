afb18375703d735a6c88413646018cc9efc12c5e
Bruno Menegidio <bruno.silva@conecta.tech>
2023-06-19 19:53:07 -0300

    feat: test layout

--- apps/coins-app/src/app/App.tsx
+++ apps/coins-app/src/app/App.tsx
@@ -1,22 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout } from '@ui-kitten/components';
+import {
+  ApplicationProvider,
+  BottomNavigation,
+  BottomNavigationProps,
+  BottomNavigationTab,
+  Divider,
+  Icon,
+  IconElement,
+  IconRegistry,
+  Layout,
+  TopNavigation,
+  TopNavigationAction,
+} from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
-import { default as theme } from '../theme/ui-kitten-theme.json';
+import { default as evaTheme } from '../theme/ui-kitten-theme.json';
 
 import AssetsPage from './pages/assets';
 
+const useBottomNavigationState = (initialState = 0): BottomNavigationProps => {
+  const [selectedIndex, setSelectedIndex] = React.useState(initialState);
+  return { selectedIndex, onSelect: setSelectedIndex };
+};
+
 export const App = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const bottomState = useBottomNavigationState();
+
+  const moonIcon = (props: any): IconElement => (
+    <Icon name="moon-outline" {...props} />
+  );
+
+  const sunIcon = (props: any): IconElement => (
+    <Icon name="sun-outline" {...props} />
+  );
+
+  const renderToggleThemeAction = (): React.ReactElement => (
+    <TopNavigationAction
+      icon={theme === 'light' ? moonIcon : sunIcon}
+      onPress={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+    />
+  );
+
   return (
-    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
-      <StatusBar barStyle="light-content" />
-      <Layout style={{ flex: 1 }}>
-        <SafeAreaView>
-          <AssetsPage />
-        </SafeAreaView>
-      </Layout>
-    </ApplicationProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={{ ...eva[theme], ...evaTheme }}>
+        <StatusBar barStyle="dark-content" />
+        <Layout style={{ flex: 1 }}>
+          <SafeAreaView style={{ flex: 1 }}>
+            <Layout style={{ flex: 1 }}>
+              <TopNavigation
+                alignment="center"
+                title="Coins APP"
+                // accessoryLeft={renderBackAction}
+                accessoryRight={renderToggleThemeAction}
+              />
+              <Divider />
+              <AssetsPage />
+            </Layout>
+            <BottomNavigation {...bottomState}>
+              <BottomNavigationTab title="Moedas" icon={moonIcon} />
+              <BottomNavigationTab title="Configs" icon={sunIcon} />
+            </BottomNavigation>
+          </SafeAreaView>
+        </Layout>
+      </ApplicationProvider>
+    </>
   );
 };
 
