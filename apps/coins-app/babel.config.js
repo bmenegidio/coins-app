1c3d39d8a6989e8234b4705ff5767a8ac44aa2f9
Bruno Menegidio <bruno.silva@conecta.tech>
2023-06-22 16:45:56 -0300

    feat: env config & .env.example

--- apps/coins-app/babel.config.js
+++ apps/coins-app/babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [require.resolve('expo-router/babel')],
+    plugins: [
+      require.resolve('expo-router/babel'),
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
