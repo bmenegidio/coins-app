f6e8dbb568b05c60d840e2fc47660d803aedf4db
Bruno Menegidio <bruno.silva@conecta.tech>
2023-06-24 18:12:06 -0300

    test: assets controller

---
+++ apps/coins-api/src/app/modules/assets/assets.controller.spec.ts
@@ -0,0 +1,38 @@
+import { Test } from '@nestjs/testing';
+import { firstValueFrom, of } from 'rxjs';
+
+import { AssetsController } from '@/modules/assets/assets.controller';
+import { AssetsService } from '@/modules/assets/assets.service';
+
+describe('AssetsController', () => {
+  let assetsController: AssetsController;
+  let assetsService: AssetsService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [AssetsController],
+      providers: [
+        {
+          provide: AssetsService,
+          useValue: {
+            getAssets: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    assetsController = moduleRef.get<AssetsController>(AssetsController);
+    assetsService = moduleRef.get<AssetsService>(AssetsService);
+  });
+
+  it('should be defined', () => {
+    expect(assetsController).toBeDefined();
+    expect(assetsService).toBeDefined();
+  });
+
+  it('should return an empty array of assets', async () => {
+    jest.spyOn(assetsService, 'getAssets').mockImplementationOnce(() => of([]));
+    expect(await firstValueFrom(assetsController.getAssets())).toEqual([]);
+    expect(assetsService.getAssets).toBeCalledTimes(1);
+  });
+});
