3cb37339e8cccfa8f4b9c540773d077e7da87149
Bruno Menegidio <bruno.silva@conecta.tech>
2023-07-04 17:52:36 -0300

    test: get asset by id

--- apps/coins-api/src/app/services/coin-api/coin-api.service.spec.ts
+++ apps/coins-api/src/app/services/coin-api/coin-api.service.spec.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AxiosResponse } from 'axios';
 import { firstValueFrom, of } from 'rxjs';
@@ -12,6 +12,38 @@ import { CoinApiService } from './coin-api.service';
 describe('CoinApiService', () => {
   let service: CoinApiService;
   let httpService: HttpService;
+
+  const coinApiAssetsResponse: AxiosResponse<CoinApiAssetsResponseRo> = {
+    data: [
+      {
+        asset_id: 'BTC',
+        name: 'Bitcoin',
+        type_is_crypto: 1,
+        data_start: '2010-07-17',
+        data_end: '2019-11-03',
+        data_quote_start: '2014-02-24T17:43:05.0000000Z',
+        data_quote_end: '2019-11-03T17:55:07.6724523Z',
+        data_orderbook_start: '2014-02-24T17:43:05.0000000Z',
+        data_orderbook_end: '2019-11-03T17:55:17.8592413Z',
+        data_trade_start: '2010-07-17T23:09:17.0000000Z',
+        data_trade_end: '2019-11-03T17:55:11.8220000Z',
+        data_symbols_count: 22711,
+        volume_1hrs_usd: 102894431436.49,
+        volume_1day_usd: 2086392323256.16,
+        volume_1mth_usd: 57929168359984.54,
+        price_usd: 9166,
+      },
+    ],
+  } as AxiosResponse;
+  const assetsNormalizedResults: AssetsRo[] = [
+    {
+      id: 'BTC',
+      label: 'Bitcoin (BTC)',
+      name: 'Bitcoin',
+      priceUsd: '$9,166.000',
+      volume1HrsUsd: 102894431436.49,
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,43 +63,32 @@ describe('CoinApiService', () => {
   });
 
   it('should return normalized assets', async () => {
-    const coinApiAssetsResponse: AxiosResponse<CoinApiAssetsResponseRo> = {
-      data: [
-        {
-          asset_id: 'BTC',
-          name: 'Bitcoin',
-          type_is_crypto: 1,
-          data_start: '2010-07-17',
-          data_end: '2019-11-03',
-          data_quote_start: '2014-02-24T17:43:05.0000000Z',
-          data_quote_end: '2019-11-03T17:55:07.6724523Z',
-          data_orderbook_start: '2014-02-24T17:43:05.0000000Z',
-          data_orderbook_end: '2019-11-03T17:55:17.8592413Z',
-          data_trade_start: '2010-07-17T23:09:17.0000000Z',
-          data_trade_end: '2019-11-03T17:55:11.8220000Z',
-          data_symbols_count: 22711,
-          volume_1hrs_usd: 102894431436.49,
-          volume_1day_usd: 2086392323256.16,
-          volume_1mth_usd: 57929168359984.54,
-          price_usd: 9166,
-        },
-      ],
-    } as AxiosResponse;
-    const assetsNormalizedResults: AssetsRo[] = [
-      {
-        id: 'BTC',
-        label: 'Bitcoin (BTC)',
-        name: 'Bitcoin',
-        priceUsd: '$9,166.000',
-        volume1HrsUsd: 102894431436.49,
-      },
-    ];
-
     jest
       .spyOn(httpService, 'get')
       .mockImplementationOnce(() => of(coinApiAssetsResponse));
     const assets = await firstValueFrom(service.getAssets());
     expect(assets).toEqual(assetsNormalizedResults);
+  });
+
+  it('should return normalized asset getById', async () => {
+    jest
+      .spyOn(httpService, 'get')
+      .mockImplementationOnce(() => of(coinApiAssetsResponse));
+    const asset = await firstValueFrom(service.getById('BTC'));
+    expect(asset).toEqual(assetsNormalizedResults.at(0));
+  });
+
+  it('should return instance of NotFoundException when coinApiAssetsResponse is empty array', async () => {
+    const coinApiAssetsResponse: AxiosResponse<CoinApiAssetsResponseRo> = {
+      data: [],
+    } as AxiosResponse;
+
+    jest
+      .spyOn(httpService, 'get')
+      .mockImplementationOnce(() => of(coinApiAssetsResponse));
+    await expect(firstValueFrom(service.getById('BTC'))).rejects.toThrow(
+      NotFoundException
+    );
   });
 
   it('should return instance of BadRequestException when coinApiAssetsResponse is null', async () => {
